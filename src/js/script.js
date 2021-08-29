1ee2af086e40f77151c31d2a3add9639399631a8
Martin Dobrev <martindobrev87@gmail.com>
2021-08-29 20:07:04 +0300

    Defi Demo added

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,55 @@
-(async function () {
-  const Web3Modal = window.Web3Modal.default;
-  const providerOptions = {
-    /* See Provider Options Section */
-  };
+const inputIds = ['#number-x', '#number-y'];
+const $inputInitialX = $('#number-initial-x');
+const $inputX = $('#number-x');
+const $priceX = $('#price-x');
+const $inputInitialY = $('#number-initial-y');
+const $inputY = $('#number-y');
+const $priceY = $('#price-y');
+const $inputK = $('#number-k');
 
-  const web3Modal = new Web3Modal({
-    network: 'mainnet', // optional
-    cacheProvider: true, // optional
-    providerOptions, // required
+const ftNum = (inputValue) => {
+  return parseInt(inputValue.trim(), 10);
+};
+
+const updateK = () => {
+  const valueX = ftNum($inputInitialX.val());
+  const valueY = ftNum($inputInitialY.val());
+
+  $inputK.val(valueX * valueY);
+  $inputX.val(valueX);
+  $inputY.val(valueY);
+  $priceX.html(valueY / valueX);
+  $priceY.html(valueX / valueY);
+};
+
+const updateFormula = (e) => {
+  // Get inputs ids
+  const updatedInputId = `#${e.target.id}`;
+  let otherInputId;
+  inputIds.forEach((inputId) => {
+    if (inputId !== updatedInputId) {
+      otherInputId = inputId;
+    }
   });
 
-  const provider = await web3Modal.connect();
-})();
+  // Get inputs value based on formula
+  const updatedInputValue = ftNum($(updatedInputId).val());
+  const otherInputValue = ftNum($inputK.val()) / updatedInputValue;
+
+  // Get new prices
+  const updatedInputPrice = otherInputValue / updatedInputValue;
+  const otherInputPrice = updatedInputValue / otherInputValue;
+
+  // Update input prices
+  $(updatedInputId).parent().find('.price-tag').html(updatedInputPrice);
+  $(otherInputId).parent().find('.price-tag').html(otherInputPrice);
+
+  // Update other input value
+  inputIds.forEach((inputId) => inputId !== updatedInputId && $(inputId).val(otherInputValue));
+};
+
+$(() => {
+  updateK();
+  $('.js-update-k').on('change', updateK);
+  $('.js-update-formula').on('change', updateFormula);
+});
